9ba8203d50b5191322a882ea1b41a8c6386eae1c
Long Do Cao <ldocao@gmail.com>
2015-01-27 17:42:16 +0900

    Modify README markdown Add setup files Add constants.py

---
+++ pygadgettools/constants.py
@@ -0,0 +1,19 @@
+### NAME: constants.py
+### PURPOSE: define constants variables in cgs units.
+
+import numpy as np
+
+## Universal constants
+PARSEC                 = 3.08567758e16
+SOLAR_MASS             = 1.9891e30
+BOLTZMANN_CONSTANT     = 1.380658e-16
+PROTON_MASS            = 1.6726231e-24;  
+GRAVITATIONAL_CONSTANT = 6.67259e-8 
+
+## Gadget Units
+UnitLength   = 3.085678e18; #             Kpc --> cm
+UnitMass     = 1.989e43;    # M_solar * 10^10 --> grams
+UnitVelocity = 1.0e5;       #            km/s --> cm/s
+UnitTime     = UnitLength/UnitVelocity; #Seconds
+UnitEnergy   = UnitMass * UnitLength * UnitLength / (UnitTime*UnitTime); #Erg
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pygadgettools',
+      version='0.1',
+      description='Toolbox with python scripts and function for basic analysis of GADGET data.',
+      url='https://github.com/ldocao/pygadgettools',
+      author='Long DO CAO',
+      author_email='ldocao@gmail.com',
+      license='None',
+      packages=['pygadgettools'],
+      zip_safe=False)
