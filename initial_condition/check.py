8537597cb03af6cde3d14b9caf13334acfc7546b
ldocao <ldocao@gmail.com>
2015-03-04 21:45:42 +0900

    check that the number of particles is not zero in the header

--- initial_condition/check.py
+++ initial_condition/check.py
@@ -1,20 +1,72 @@
-# def check_if_file_exists(filename):
-#     """Check if the file exists in the current directory
+import sys
 
-#     """
-#     check=True
+def check_if_file_exists(filename):
+    """Check if the file exists in the current directory
 
-#     return check
+    """
+
+    return None
 
 
-# def check_header(header):
-#     """Run a series of tests to check the header
+def check_header(Header):
+    """Run a series of tests to check the header
 
-#     """
-#     return None
+    """
+    print "Checking header..."
+    ##check if there are some particles
+    if (np.sum(Header.NumPart_ThisFile) == 0) or (Header.NumPart_Total == 0):
+        print "No particles in header !"
+        sys.exit()
 
-# def check_body(body):
-#     """Run a series of tests to check the body
+
+    return None
+
+def check_body(Body):
+    """Run a series of tests to check the body
     
-#     """
-#     return None
+    """
+    return None
+
+
+
+def check_consistency(Header, Body):
+    """Run a series of test to check consistency between body and header
+
+    Parameters
+    ----------
+    
+    Header : object
+        see Class Header
+        
+    Body : object
+        see Class Body
+    """
+
+    ##check dimensions
+
+    return None
+
+
+
+def print_summary(Header,Body):
+    """Print a summary of the parameters
+    
+    Parameters
+    ----------
+    
+    header : class
+       see Header Class
+
+    body : class
+       see Body Class
+    """
+    from pprint import pprint
+
+
+    ##print header attributes
+    pprint (vars(Header))
+
+    ##print body summary
+
+    return None
+
