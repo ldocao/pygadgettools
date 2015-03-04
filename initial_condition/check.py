ea3daa0799b850a426c669bf300b5625807326f5
ldocao <ldocao@gmail.com>
2015-03-04 19:47:38 +0900

    move check functions to a new separate file

---
+++ initial_condition/check.py
@@ -0,0 +1,20 @@
+# def check_if_file_exists(filename):
+#     """Check if the file exists in the current directory
+
+#     """
+#     check=True
+
+#     return check
+
+
+# def check_header(header):
+#     """Run a series of tests to check the header
+
+#     """
+#     return None
+
+# def check_body(body):
+#     """Run a series of tests to check the body
+    
+#     """
+#     return None
